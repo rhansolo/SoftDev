2b196f1c02ad339140f9cd0dca54f9cc8abd1fd9
robinhanstuy <rhan@stuy.edu>
2018-09-13 22:35:25 -0400

    allowed user to enter input

--- 02_randomizer/robann_foleyM-hanR.py
+++ 02_randomizer/robann_foleyM-hanR.py
@@ -6,12 +6,18 @@
 import random
 
 KREWES = {'w': ['William Lu', 'Qian', 'Peter', 'Ahnaf', 'Kenny', 'Sophia', 'Sajed', 'Emily', 'Hasif', 'Brian ', 'Dennis', 'Jiayang', 'Shafali ', 'Isaac ', 'Tania', 'Derek', 'Shin', 'Vincent', 'Ricky', 'Puneet', 'Wei Wen', 'Tim', 'Jeffrey', 'Joyce ', 'Mohtasim', 'Simon', 'Thomas', 'Ray', 'Jack', 'Karen', 'Robin', 'Jabir', 'Johnny ', 'Matthew', 'Johnson Li', 'Angela', 'Crystal', 'Jiajie', 'Theodore (Dont really care)', 'Anton', 'Max', 'Bo', 'Andrew', 'Kendrick', 'Kevin', 'Kyle', 'Jamil', 'Mohammed', 'Ryan', 'Jason'],'m': ['Daniel', 'Aleksandra', 'Addison', 'Hui Min', 'Aaron', 'Rubin', 'Raunak', 'Stefan', 'Cheryl', 'Cathy', 'Mai', 'Claire ', 'Alex', 'Bill', 'Daniel', 'Jason'],'x': ['Derek', 'Britni', 'Joan', 'Vincent', 'Jared', 'Ivan', 'Thomas', 'Maggie', 'Damian', 'Tina', 'Fabiha', 'John', 'Susan ', 'Kaitlin', 'Michelle', 'Clara', 'Rachel', 'Amit', 'Jerry', 'Raymond', 'Zane', 'Soojin', 'Maryann', 'Adil', 'Josh', 'Imad']}
-
+def main():
+	ans = ""
+	#allows user to input a crew!
+	ans = input('Pick a krew! (w/m/x) \n')
+	# input must be a single character which is either w , x , m 	
+	if ans in "wxm" and len(ans) == 1:
+		print (randFam(ans))
+	else : 
+	# if crew doesn't match standards, reruns main and asks the user to input a valid option again
+		print ("Not valid, try again!")
+		main()
 def randFam(team):
-    student=random.choice(KREWES[team])
-    print(student)
+    return random.choice(KREWES[team])
 
-#randFam("w")
-#randFam("m")
-#randFam("x")
-
+main()
