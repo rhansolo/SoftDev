971b15cf36706630b3f6940490046248a3e45d98
robinhanstuy <rhan@stuy.edu>
2018-10-02 22:11:48 -0400

    flashing complete!

--- 15_flashinh/app.py
+++ 15_flashinh/app.py
@@ -15,6 +15,8 @@ users = {"slin15": "12345"}
 #home root
 @app.route('/', methods=["POST", "GET"])
 def home():
+     if 'slin15' in session:
+          return ('return.html')
      return render_template('form.html')
  
 #reading in user and password, checking to see if it is valid or not
@@ -27,13 +29,12 @@ def login():
     #username doesn't match
     elif request.args['user'] not in users.keys():
         flash("username not found")
-        #return render_template('error.html', error="username not found")
+        return render_template('form.html', error=True)
 
     #password doesn't match username        
     elif users[request.args['user']] != request.args['pass']:
         flash("password incorrect")
-        #return render_template('error.html', error="incorrect password")
-    return redirect(url_for('home'))
+        return render_template('form.html', error=True)
 
 # logout route, sends user back to home root and forgets current user
 @app.route("/logout", methods=["POST", "GET"])
