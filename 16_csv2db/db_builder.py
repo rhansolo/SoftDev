d8357ea65bcd91c362bc93c89a7aee98fe49855f
robinhanstuy <rhan@stuy.edu>
2018-10-04 22:02:11 -0400

    hw 16 finished

--- 16_csv2db/db_builder.py
+++ 16_csv2db/db_builder.py
@@ -1,6 +1,6 @@
-#Clyde "Thluffy" Sinclair
-#SoftDev1 pd0
-#SQLITE3 BASICS
+#Quacking Geese: Maryann Foley and Robin Han
+#SoftDev1 pd8
+#K16:No Trouble
 #2018-10-04
 
 import sqlite3   #enable control of an sqlite database
@@ -19,7 +19,7 @@ c = db.cursor()               #facilitate db ops
 command = ""
 with open("peeps.csv", newline = '\n') as csvfile:
     reader=csv.DictReader(csvfile)
-    #command+="CREATE TABLE peeps (name TEXT, id INTEGER, age INTEGER);"
+    command+="CREATE TABLE peeps (name TEXT, id INTEGER, age INTEGER);"
     for row in reader:
             command+="INSERT INTO peeps VALUES ('"+row['name']+"', "+row['id']+", "+row['age']+");"
             
@@ -28,14 +28,12 @@ with open("peeps.csv", newline = '\n') as csvfile:
 
 with open("courses.csv", newline = '\n') as csvfile2:
     reader=csv.DictReader(csvfile2)
-    #command+="CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
+    command+="CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
     for row in reader:
-            command+="INSERT INTO peeps VALUES ('"+row['code']+"', "+row['id']+", "+row['mark']+");"
+            command+="INSERT INTO courses VALUES ('"+row['code']+"', "+row['id']+", "+row['mark']+");"
 
 c.executescript(command)    #run SQL statement2
 #==========================================================
 
 db.commit() #save changes
-db.close()  #close database
-
-
+db.close() #close database
