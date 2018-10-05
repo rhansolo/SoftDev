c1eff0a8ff8a84dfea90d15459b1e4e5c001d6d2
robinhanstuy <rhan@stuy.edu>
2018-10-04 21:16:46 -0400

    sqlite3 table working?

---
+++ 17_csv/db_builder.py
@@ -0,0 +1,41 @@
+#Clyde "Thluffy" Sinclair
+#SoftDev1 pd0
+#SQLITE3 BASICS
+#2018-10-04
+
+import sqlite3   #enable control of an sqlite database
+import csv       #facilitates CSV I/O
+
+
+
+DB_FILE="discobandit.db"
+
+db = sqlite3.connect(DB_FILE) #open if file exists, otherwise create
+c = db.cursor()               #facilitate db ops
+
+#==========================================================
+#INSERT YOUR POPULATE CODE IN THIS ZONE
+
+command = ""
+with open("peeps.csv", newline = '\n') as csvfile:
+    reader=csv.DictReader(csvfile)
+    #command+="CREATE TABLE peeps (name TEXT, id INTEGER, age INTEGER);"
+    for row in reader:
+            command+="INSERT INTO peeps VALUES ('"+row['name']+"', "+row['id']+", "+row['age']+");"
+            
+#####c.execute(command)    #run SQL statement only executes one at a time
+ ##create second table
+
+with open("courses.csv", newline = '\n') as csvfile2:
+    reader=csv.DictReader(csvfile2)
+    #command+="CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER);"
+    for row in reader:
+            command+="INSERT INTO peeps VALUES ('"+row['code']+"', "+row['id']+", "+row['mark']+");"
+
+c.executescript(command)    #run SQL statement2
+#==========================================================
+
+db.commit() #save changes
+db.close()  #close database
+
+
